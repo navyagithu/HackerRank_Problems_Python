a6237cfb32a0c1f10d52600c8f911d24046a1f64
Navya Kranthi M <126189684+navyagithu@users.noreply.github.com>
2025-01-04 22:09:21 +0530

    simplify the code

--- 1_EASY/8_captalize.py
+++ 1_EASY/8_captalize.py
@@ -4,36 +4,18 @@ You are asked to ensure that the first and last names of people begin with a cap
 
 Given a full name, your task is to capitalize the name appropriately.
 STATUS: SOLVED
-LINK: https://www.hackerrank.com/challenges/capitalize/problem?isFullScreen=true
-SOLUTION:
+LINK: 
+SOLUTION: https://www.hackerrank.com/challenges/capitalize/problem?isFullScreen=true
 '''
-#!/bin/python3
-
-# Import necessary libraries
-import math  # Standard math library (not used in this script)
-import os  # Provides functionality to interact with the operating system
-import random  # For generating random numbers (not used in this script)
-import re  # Regular expression library (not used in this script)
-import sys  # Provides system-specific parameters and functions
-
-# Define the solve function to capitalize each word in the input string
 def solve(s):
     # Split the string by spaces, capitalize each word, and join them back with spaces
     return " ".join([i.capitalize() for i in s.split(' ')])
 
 # Main execution starts here
 if __name__ == '__main__':
-    # Open a file for writing output; the file path is specified in the environment variable 'OUTPUT_PATH'
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     # Take input string from the user
     s = input()
 
     # Call the solve function to process the input and get the result
     result = solve(s)
-
-    # Write the result to the output file followed by a newline
-    fptr.write(result + '\n')
-
-    # Close the output file
-    fptr.close()
+    print(result)
